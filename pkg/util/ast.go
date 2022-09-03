82e27d01745c749c40742b48a3b74f9f9beded64
Galaxyobe <galaxyobe@qq.com>
2022-09-03 13:00:37 +0800

    fix: short func name

--- pkg/util/ast.go
+++ pkg/util/ast.go
@@ -37,91 +37,6 @@ func AstParseDir(dir string) (files []*ast.File, err error) {
 	return
 }
 
-func getSpecKindFieldsInGenDecl(genDecl *ast.GenDecl, kind string) map[string][]string {
-	var m = make(map[string][]string)
-	for _, spec := range genDecl.Specs {
-		switch s := spec.(type) {
-		case *ast.TypeSpec:
-			name, list := getSpecKindFieldsInTypeSpec(s, kind)
-			if len(list) == 0 {
-				continue
-			}
-			exists := m[name]
-			if exists == nil {
-				m[name] = list
-			} else {
-				exists = append(exists, list...)
-				m[name] = exists
-			}
-		}
-	}
-	return m
-}
-
-func getSpecKindFieldsInTypeSpec(typeSpec *ast.TypeSpec, kind string) (name string, fields []string) {
-	name = typeSpec.Name.Name
-	switch t := typeSpec.Type.(type) {
-	case *ast.StructType:
-		fields = append(fields, getSpecKindFieldsInStructType(t, kind)...)
-	case *ast.Ident:
-		fields = append(fields, getSpecKindFieldsInIdent(t, kind)...)
-	default:
-		return
-	}
-	return
-}
-
-func getSpecKindFieldsInStructType(structType *ast.StructType, kind string) (fields []string) {
-	for _, field := range structType.Fields.List {
-		switch f := field.Type.(type) {
-		case *ast.Ident:
-			if f.Name == kind {
-				if len(field.Names) == 0 {
-					continue
-				}
-				fields = append(fields, field.Names[0].Name)
-			}
-		}
-	}
-	return
-}
-
-func getSpecKindFieldsInIdent(ident *ast.Ident, kind string) (fields []string) {
-	if ident.Obj == nil {
-		return
-	}
-	switch decl := ident.Obj.Decl.(type) {
-	case *ast.TypeSpec:
-		_, fields = getSpecKindFieldsInTypeSpec(decl, kind)
-	}
-	return
-}
-
-func FindStructSpecKindFields(files []*ast.File, kind string) map[string][]string {
-	var m = make(map[string][]string)
-	for _, file := range files {
-		ast.Inspect(file, func(node ast.Node) bool {
-			switch n := node.(type) {
-			case *ast.GenDecl:
-				result := getSpecKindFieldsInGenDecl(n, kind)
-				for k, v := range result {
-					m[k] = v
-				}
-			}
-			return true
-		})
-	}
-	return m
-}
-
-func FindUint8Type(files []*ast.File) map[string][]string {
-	return FindStructSpecKindFields(files, "uint8")
-}
-
-func FindInt8Type(files []*ast.File) map[string][]string {
-	return FindStructSpecKindFields(files, "int8")
-}
-
 func getAstObjectName(o any) string {
 	switch v := o.(type) {
 	case *ast.Ident:
@@ -156,7 +71,7 @@ func getFuncType(funcType *ast.FuncType) string {
 	return buf.String()
 }
 
-func getPackageTypeInTypeSpec(typeSpec *ast.TypeSpec) (type_ string, name string) {
+func getTypeInTypeSpec(typeSpec *ast.TypeSpec) (type_ string, name string) {
 	name = typeSpec.Name.Name
 	switch t := typeSpec.Type.(type) {
 	case *ast.SelectorExpr:
@@ -176,12 +91,12 @@ func getPackageTypeInTypeSpec(typeSpec *ast.TypeSpec) (type_ string, name string
 	return
 }
 
-func getPackageTypesInGenDecl(genDecl *ast.GenDecl) map[string]string {
+func getTypesInGenDecl(genDecl *ast.GenDecl) map[string]string {
 	var m = make(map[string]string)
 	for _, spec := range genDecl.Specs {
 		switch s := spec.(type) {
 		case *ast.TypeSpec:
-			type_, name := getPackageTypeInTypeSpec(s)
+			type_, name := getTypeInTypeSpec(s)
 			m[name] = type_
 		}
 	}
@@ -194,7 +109,7 @@ func FindTypeNames(files []*ast.File) (m map[string]string) {
 		ast.Inspect(file, func(node ast.Node) bool {
 			switch n := node.(type) {
 			case *ast.GenDecl:
-				result := getPackageTypesInGenDecl(n)
+				result := getTypesInGenDecl(n)
 				for k, v := range result {
 					m[k] = v
 				}
